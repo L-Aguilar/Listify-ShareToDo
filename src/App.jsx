44d7a50b7a9227c08ecb23cf4e058c0324865f01
Luis Aguilar <laaz13@gmail.com>
2021-02-13 07:24:11 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
-            
+            <NewList />
           </Route>
           <Route path="/newList">
             <NewList />
