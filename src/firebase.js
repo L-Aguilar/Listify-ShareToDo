213f59e10205f63d819a8d913ab4c19671692a5f
Luis Aguilar <laaz13@gmail.com>
2021-02-13 07:22:14 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBlaAg6YN7qRYly6jDHhuejkc7meRvrglc",
-    authDomain: "simplist-e0633.firebaseapp.com",
-    databaseURL: "https://simplist-e0633.firebaseio.com",
-    projectId: "simplist-e0633",
-    storageBucket: "simplist-e0633.appspot.com",
-    messagingSenderId: "475340693853",
-    appId: "1:475340693853:web:a44e5d2303de6540ee48b4"
+    ## Agregar tus credenciales
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
